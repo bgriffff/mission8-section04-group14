125dd0bc8daceae68b1c16139852c26357814419
lololikes8 <laurenlikes@gmail.com>
2023-02-21 21:08:05 -0700

    1st try

---
+++ mission_8/Models/TaskResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission_8.Models
+{
+    public class TaskResponse
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        
+        [Required]
+        public string Task { get; set; }
+        public string DueDate { get; set; }
+
+        [Required]
+        public int Quadrant { get; set; }
+        public string Category { get; set; }
+        public int CategoryId { get; set; }
+        public bool Completed { get; set; }
+    }
+}
